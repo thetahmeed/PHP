3ac96fa26a0b86687be2f2620772e4f23cc7715a
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-01-07 13:43:10 +0600

    Create php003.1VariablesScope.php

---
+++ php003.1VariablesScope.php
@@ -0,0 +1,46 @@
+<?php
+$x = 5; // global scope
+
+function myTest() {
+  // using x inside this function will generate an error
+  echo "<p>Variable x inside function is: $x</p>";
+}
+myTest();
+
+echo "<p>Variable x outside function is: $x</p>";
+
+// PHP The global Keyword
+$x = 5;
+$y = 10;
+
+function myTest() {
+  global $x, $y;
+  $y = $x + $y;
+}
+
+myTest();
+echo $y; // outputs 15
+
+// or
+$x = 5;
+$y = 10;
+
+function myTest() {
+  $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
+}
+
+myTest();
+echo $y; // outputs 15
+
+// PHP The static Keyword
+function myTest() {
+  static $x = 0;
+  echo $x;
+  $x++;
+}
+
+myTest();
+myTest();
+myTest();
+
+?>
