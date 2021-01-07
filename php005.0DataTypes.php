befcd61d7decaefe9c8e4908ed36f1edc22a920e
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-01-07 13:50:03 +0600

    Create php005.0DataTypes.php

---
+++ php005.0DataTypes.php
@@ -0,0 +1,33 @@
+<?php
+
+
+// String
+$x = "Hello world!";
+$y = 'Hello world!';
+
+echo $x;
+echo "<br>";
+echo $y;
+
+// Integer
+$x = 5985;
+var_dump($x);
+
+// Float
+$x = 10.365;
+var_dump($x);
+
+// Boolean
+$x = true;
+$y = false;
+
+// Array
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+
+// Null
+$x = "Hello world!";
+$x = null;
+var_dump($x);
+
+?>
