85897a27e3011e987d11ffdb3d92f378a37e656f
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-01-07 13:40:32 +0600

    Create php003Variables.php

---
+++ php003Variables.php
@@ -0,0 +1,25 @@
+<?php
+
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+
+/*
+A variable starts with the $ sign, followed by the name of the variable
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive ($age and $AGE are two different variables)
+*/
+
+$txt = "W3Schools.com";
+echo "I love $txt!";
+
+$txt = "W3Schools.com";
+echo "I love " . $txt . "!";
+
+$x = 5;
+$y = 4;
+echo $x + $y;
+
+?>
