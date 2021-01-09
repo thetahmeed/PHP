46a6e5157aa6d5cc733b4bd462d034c4a9511da1
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-01-09 20:49:56 +0600

    Update php007Operators.php

--- php007Operators.php
+++ php007Operators.php
@@ -20,6 +20,18 @@ echo $x + $y;
 // x /= y	x = x / y	Division	
 // x %= y	x = x % y	Modulus
 
+// PHP Comparison Operators
+// ==	Equal	$x == $y	Returns true if $x is equal to $y	
+// ===	Identical	$x === $y	Returns true if $x is equal to $y, and they are of the same type	
+// !=	Not equal	$x != $y	Returns true if $x is not equal to $y	
+// <>	Not equal	$x <> $y	Returns true if $x is not equal to $y	
+// !==	Not identical	$x !== $y	Returns true if $x is not equal to $y, or they are not of the same type	
+// >	Greater than	$x > $y	Returns true if $x is greater than $y	
+// <	Less than	$x < $y	Returns true if $x is less than $y	
+// >=	Greater than or equal to	$x >= $y	Returns true if $x is greater than or equal to $y	
+// <=	Less than or equal to	$x <= $y	Returns true if $x is less than or equal to $y	
+// <=>	Spaceship	$x <=> $y	Returns an integer less than, equal to, or greater than zero, depending on if $x is less than, equal to, or greater than $y. Introduced in PHP 7.
+
 
 
 ?>
