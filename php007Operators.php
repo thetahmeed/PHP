1807b04132290543d094b21e237e54de6940a451
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-01-09 20:48:32 +0600

    Create php007Operators.php

---
+++ php007Operators.php
@@ -0,0 +1,17 @@
+<?php
+$x = 10;  
+$y = 6;
+
+echo $x + $y;
+
+// PHP Arithmetic Operators
+// +	Addition	$x + $y	Sum of $x and $y	
+// -	Subtraction	$x - $y	Difference of $x and $y	
+// *	Multiplication	$x * $y	Product of $x and $y	
+// /	Division	$x / $y	Quotient of $x and $y	
+// %	Modulus	$x % $y	Remainder of $x divided by $y	
+// **	Exponentiation	$x ** $y	Result of raising $x to the $y'th power
+
+
+
+?>
