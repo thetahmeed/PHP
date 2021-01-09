79acc8b3c3bde775c63a84cac4c5202bed98cfcd
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-01-09 20:49:14 +0600

    Update php007Operators.php

--- php007Operators.php
+++ php007Operators.php
@@ -12,6 +12,14 @@ echo $x + $y;
 // %	Modulus	$x % $y	Remainder of $x divided by $y	
 // **	Exponentiation	$x ** $y	Result of raising $x to the $y'th power
 
+// PHP Assignment Operators
+// x = y	x = y	The left operand gets set to the value of the expression on the right	
+// x += y	x = x + y	Addition	
+// x -= y	x = x - y	Subtraction	
+// x *= y	x = x * y	Multiplication	
+// x /= y	x = x / y	Division	
+// x %= y	x = x % y	Modulus
+
 
 
 ?>
