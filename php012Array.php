9125b47d727abb51a075796d5ae7ffb35847d00e
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-01-11 11:35:38 +0600

    Update php012Array.php

--- php012Array.php
+++ php012Array.php
@@ -34,5 +34,14 @@ echo $cars[1][0].": In stock: ".$cars[1][1].", sold: ".$cars[1][2].".<br>";
 echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
 echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br>";
 
+//sorting asort() - sort arrays in ascending order
+// rsort() - sort arrays in descending order
+// asort() - sort associative arrays in ascending order, according to the value
+// ksort() - sort associative arrays in ascending order, according to the key
+// arsort() - sort associative arrays in descending order, according to the value
+// krsort() - sort associative arrays in descending order, according to the keyrray
+
+$cars = array("Volvo", "BMW", "Toyota");
+sort($cars);
 
 ?>
