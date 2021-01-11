e2f85817623813ceafb3a9557df817a7e6649f5d
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-01-11 11:30:17 +0600

    Update php012Array.php

--- php012Array.php
+++ php012Array.php
@@ -8,6 +8,13 @@ echo count($cars);
 $cars = array("Volvo", "BMW", "Toyota");
 echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
 
+// get the value by a loop
+$cars = array("Volvo", "BMW", "Toyota");
+$arrlength = count($cars);
 
+for($x = 0; $x < $arrlength; $x++) {
+  echo $cars[$x];
+  echo "<br>";
+}
 
 ?>
