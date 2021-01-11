59106b2dcad45e7a5a27ff73f99e315c477fee9c
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-01-11 11:10:43 +0600

    Update php012Array.php

--- php012Array.php
+++ php012Array.php
@@ -1,7 +1,13 @@
 <?php
 
+// size of the array
 $cars = array("Volvo", "BMW", "Toyota");
-echo count($cars);                          // size of the array
+echo count($cars);                          
+
+// getting array by index number
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+
 
 
 ?>
